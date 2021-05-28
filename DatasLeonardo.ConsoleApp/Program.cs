0b6567d7cd475983db2c678d54f6f31b13402025
Leonardo Meister <leozinhomm1@gmail.com>
2021-05-28 16:02:08 -0300

    Att

--- DatasLeonardo.ConsoleApp/Program.cs
+++ DatasLeonardo.ConsoleApp/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using DatasLeonardo;
+using Cell_Leonardo;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DatasLeonardo.ConsoleApp
+namespace Cell_Leonardo.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Celular aux = new Celular("SEMPRE ACESSO O DOJOPUZZLES");
+            Celular aux = new Celular("aeiou");
+            Console.WriteLine(aux.Traducao);
         }
 
     }
--- DatasLeonardo.Test/TesteClassPeriodoPassado.cs
+++ DatasLeonardo.Test/TesteClassPeriodoPassado.cs
@@ -2,22 +2,58 @@
 using System;
 using DatasLeonardo;
 
-namespace DatasLeonardo.Test
+
+namespace DatasLeonardo.ConsoleApp
 {
     [TestClass]
     public class TesteClassPeriodoPassado
     {
         [TestMethod]
-        public void DeveRetornarAnoMesSemanaDia()
+        public void DeveRetornarCodigoEsperado()
         {
-            PeriodoPassado aux;
-            aux = new PeriodoPassado(new DateTime(2001, 08, 17));
-            string auxString = aux.StringDataExtenso;
-            Assert.AreEqual("Dezenove Anos, Nove Meses, Duas Semanas e Tres Dias", auxString);
+            Celular aux;
+                           
+            aux = new Celular("SEMPRE ACESSO O DOJOPUZZLES");
+            string auxString = aux.Traducao;
+            Assert.AreEqual("77773367_7773302_222337777_777766606660366656667889999_9999555337777", auxString);
 
-            aux = new PeriodoPassado(new DateTime(2001, 07, 20));
-            auxString = aux.StringDataExtenso;
-            Assert.AreEqual("Dezenove Anos, Dez Meses, Duas Semanas e Um Dia", auxString);
+            aux = new Celular("LEEONARDO  MARGOTI MEISTER");
+            auxString = aux.Traducao;
+            Assert.AreEqual("55533_33666_66277736660_0627774666844406334447777833777", auxString);
+
+            aux = new Celular("LEEONARDO  MARGOTI MEISTER");
+            auxString = aux.Traducao;
+            Assert.AreEqual("55533_33666_66277736660_0627774666844406334447777833777", auxString);
+        }
+
+        [TestMethod]
+        public void DeveRetornarCodigoEsperadoEspacos()
+        {
+            Celular aux;
+
+            aux = new Celular("  a  ");
+            string auxString = aux.Traducao;
+            Assert.AreEqual("0_020_0", auxString);           
+        }
+
+        [TestMethod]
+        public void DeveRetornarCodigoEsperadoConsoantes()
+        {
+            Celular aux;
+
+            aux = new Celular("bcdfghjklmnpqrstvwxz");
+            string auxString = aux.Traducao;
+            Assert.AreEqual("22_2223_3334_445_55_5556_667_77_777_77778_8889_99_9999", auxString);
+        }
+
+        [TestMethod]
+        public void DeveRetornarCodigoEsperadoVogais()
+        {
+            Celular aux;
+
+            aux = new Celular("aeiou");
+            string auxString = aux.Traducao;
+            Assert.AreEqual("23344466688", auxString);
         }
     }
 }
